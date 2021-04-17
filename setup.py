51dcd90361aef76fb55a72fa7e7a82c94e19d782
Fruktus <fruktusek@gmail.com>
2021-04-17 20:24:04 +0200

    add hybrid and weighted hybrid algorithm

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="cloudplanner",
-    version="0.0.36",
+    version="0.0.37",
     author="Fruktus",
     author_email="fruktusek@gmail.com",
     description="Cloud resource usage planner",
